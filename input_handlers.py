48dca24b5b43c90181c78299966b850bc949d1a9
Shoes01 <0jason.gilbert@gmail.com>
2018-11-20 20:14:27 -0400

    Structure input_handler more logically

--- input_handlers.py
+++ input_handlers.py
@@ -3,14 +3,8 @@ from game_states import GameStates, TurnStates
 def handle_keys(user_input, game_state, turn_state):
     if user_input:
         if game_state == GameStates.ENEMY_TURN:                 return handle_enemy_turn_keys(user_input)
-        elif game_state == GameStates.PLAYER_TURN:
-            if turn_state == TurnStates.PRE_MOVEMENT_PHASE:     return handle_player_pre_movement_keys(user_input)
-            elif turn_state == TurnStates.MOVEMENT_PHASE:       return handle_player_movement_keys(user_input)
-            elif turn_state == TurnStates.POST_MOVEMENT_PHASE:  return handle_player_post_movement_keys(user_input)
-            elif turn_state == TurnStates.PRE_ATTACK_PHASE:     return handle_player_pre_attack_keys(user_input)
-            elif turn_state == TurnStates.ATTACK_PHASE:         return handle_player_attack_keys(user_input)
-            elif turn_state == TurnStates.POST_MOVEMENT_PHASE:  return handle_player_post_attack_keys(user_input)
-
+        elif game_state == GameStates.PLAYER_TURN:              return handle_player_turn_keys(user_input)
+        
     return {}
 
 def handle_enemy_turn_keys(user_input):
@@ -19,13 +13,7 @@ def handle_enemy_turn_keys(user_input):
     
     return {}
 
-def handle_player_pre_movement_keys(user_input):
-    if user_input.key == 'ENTER' and user_input.alt:    return {'fullscreen': True}
-    elif user_input.key == 'ESCAPE':                    return {'exit': True}
-    
-    return {}
-
-def handle_player_movement_keys(user_input):
+def handle_player_turn_keys(user_input):
     if user_input.key == 'ENTER':                       return {'next_turn_phase': True}
     
     # Movement keys
@@ -34,30 +22,6 @@ def handle_player_movement_keys(user_input):
     elif user_input.key == 'LEFT':                      return {'move': (-1, 0)}
     elif user_input.key == 'RIGHT':                     return {'move': (1, 0)}
 
-    if user_input.key == 'ENTER' and user_input.alt:    return {'fullscreen': True}
-    elif user_input.key == 'ESCAPE':                    return {'exit': True}
-
-    return {}
-
-def handle_player_post_movement_keys(user_input):
-    if user_input.key == 'ENTER' and user_input.alt:    return {'fullscreen': True}
-    elif user_input.key == 'ESCAPE':                    return {'exit': True}
-    
-    return {}
-
-def handle_player_pre_attack_keys(user_input):
-    if user_input.key == 'ENTER' and user_input.alt:    return {'fullscreen': True}
-    elif user_input.key == 'ESCAPE':                    return {'exit': True}
-    
-    return {}
-
-def handle_player_attack_keys(user_input):
-    if user_input.key == 'ENTER' and user_input.alt:    return {'fullscreen': True}
-    elif user_input.key == 'ESCAPE':                    return {'exit': True}
-    
-    return {}
-
-def handle_player_post_attack_keys(user_input):
     if user_input.key == 'ENTER' and user_input.alt:    return {'fullscreen': True}
     elif user_input.key == 'ESCAPE':                    return {'exit': True}
     
